a284c891befed9381072753250e83ee1f59a2aeb
Rodion <tyrep2017@mail.ru>
2024-08-23 22:37:05 +0300

    bruh

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+#include "logLib.cpp"
+
 int main() {
-	printf("Hello world!\n);
+    for (int i = 0; i < 1000; ++i) {
+    LOG_ERROR("i am log error\n");
+    LOG_WARN("i am log warn\n");
+	LOG_INFO("i am log info\n");
+    }
+
+    LOG_MESSAGE("bruh + fail", BLUE_COLOR, "biba i boba\n");
 	return 0;
 }
